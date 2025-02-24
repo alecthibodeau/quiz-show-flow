730483a2fd1a7ca87ab7bd35a4ebd471d778e17d
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2025-02-24 17:50:55 -0500

    style: adjust pass response button to display clearly on all devices

--- src/components/ButtonResponse.vue
+++ src/components/ButtonResponse.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 
 /* Interfaces */
 import type { ClueResponse } from '@/interfaces/ClueResponse';
@@ -12,15 +13,29 @@ const props = defineProps<{
 
 const { clueResponse } = props;
 const buttonEmit = defineEmits(['onClickButton']);
+const deviceType = ref<string>('');
+const mobileDevicesNames: RegExp = /Mobile|Android|iPhone|webOS|iPad/i;
+const unicodeWhiteCircle: string = '\u25cb';
+
+onMounted(() => {
+  deviceType.value = mobileDevicesNames.test(navigator.userAgent) ?
+    'mobile' :
+    'desktop';
+});
 </script>
 
 <template>
   <button
-    :key="`response-button-${clueResponse.name}`"
+    :key="`${clueResponse.name}ResponseButton`"
     @click="buttonEmit('onClickButton', clueResponse)"
     :class="[`button-response button-${clueResponse.name}`]"
+    :aria-label="clueResponse.name"
   >
     <div></div>
-    <div></div>
+    <div :class=[deviceType]>
+      <span>
+        {{ unicodeWhiteCircle }}
+      </span>
+    </div>
   </button>
 </template>
